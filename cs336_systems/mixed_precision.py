b6b0177c78a9c17393f2e019b22ec7746d15d343
powergiant <xiaom@princeton.edu>
2026-04-28 19:37:53 -0400

    adding mixed_precision

--- cs336_systems/mixed_precision.py
+++ cs336_systems/mixed_precision.py
@@ -1,23 +1,56 @@
 import torch
+import torch.nn as nn
+from torch import autocast
+
+class ToyModel(nn.Module):
+    def __init__(self, in_features: int, out_features: int):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, 10, bias=False)
+        self.ln = nn.LayerNorm(10)
+        self.fc2 = nn.Linear(10, out_features, bias=False)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.ln(x)
+        x = self.fc2(x)
+        return x
 
 if __name__ == '__main__':
-    s = torch.tensor(0,dtype=torch.float32)
-    for i in range(1000):
-        s += torch.tensor(0.01,dtype=torch.float32)
-    print(s)
+    x = torch.randn(10, 100)
+    dtype = torch.float16
+    model = ToyModel(100, 100)
+    opt = torch.optim.Adam(model.parameters())
 
-    s = torch.tensor(0,dtype=torch.float16)
-    for i in range(1000):
-        s += torch.tensor(0.01,dtype=torch.float16)
-    print(s)
+    with autocast(device_type='cpu', dtype=torch.float16):
+        opt.zero_grad()
+        y: torch.Tensor = model(x)
+        loss = y.sum()
+        loss.backward()
+        opt.step()
 
-    s = torch.tensor(0,dtype=torch.float32)
-    for i in range(1000):
-        s += torch.tensor(0.01,dtype=torch.float16)
-    print(s)
+    x, loss, y
 
-    s = torch.tensor(0,dtype=torch.float32)
-    for i in range(1000):
-        x = torch.tensor(0.01,dtype=torch.float16)
-        s += x.type(torch.float32)
-    print(s)
+
+
+# if __name__ == '__main__':
+#     s = torch.tensor(0,dtype=torch.float32)
+#     for i in range(1000):
+#         s += torch.tensor(0.01,dtype=torch.float32)
+#     print(s)
+
+#     s = torch.tensor(0,dtype=torch.float16)
+#     for i in range(1000):
+#         s += torch.tensor(0.01,dtype=torch.float16)
+#     print(s)
+
+#     s = torch.tensor(0,dtype=torch.float32)
+#     for i in range(1000):
+#         s += torch.tensor(0.01,dtype=torch.float16)
+#     print(s)
+
+#     s = torch.tensor(0,dtype=torch.float32)
+#     for i in range(1000):
+#         x = torch.tensor(0.01,dtype=torch.float16)
+#         s += x.type(torch.float32)
+#     print(s)
