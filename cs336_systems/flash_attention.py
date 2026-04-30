e2308512682794f40b0cd8e8afd9069c040713db
powergiant <xiaom@princeton.edu>
2026-04-29 22:00:48 -0400

    writing add function

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -3,8 +3,21 @@ import triton.language as tl
 import torch
 
 @triton.jit
-def add_kernel(x_ptr, y_ptr, output, n_elem, BLOCK_SIZE):
-    print('pid', tl.program_id(0))
+def add_kernel(x_ptr, y_ptr, output_ptr, n_elem, BLOCK_SIZE: tl.constexpr):
+    pid = tl.program_id(0)
+    print('pid', pid)
+
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elem
+
+    x = tl.load(x_ptr + offsets, mask)
+    y = tl.load(y_ptr + offsets, mask)
+
+    output = x + y
+
+    tl.store(output_ptr, output, mask)
+
 
 n_elem = 4096
 x = torch.rand(n_elem)
