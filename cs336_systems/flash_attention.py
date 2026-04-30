6bf7b45c1d010a9b7bd06f48f26b23fd30e1b3f6
powergiant <xiaom@princeton.edu>
2026-04-29 21:28:22 -0400

    small prototype for printing

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -1,6 +1,14 @@
 import triton
 import triton.language as tl
+import torch
 
 @triton.jit
-def add(x_ptr, y_ptr):
-    pass
+def add_kernel(x_ptr, y_ptr, output, n_elem, BLOCK_SIZE):
+    print('pid', tl.program_id(0))
+
+n_elem = 4096
+x = torch.rand(n_elem)
+y = torch.rand(n_elem)
+output = torch.empty_like(x)
+grid = lambda meta: triton.cdiv(n_elem, meta['BLOCK_SIZE'])
+add_kernel[grid](x, y, output, n_elem, BLOCK_SIZE=1024)
