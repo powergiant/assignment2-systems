969585f8dd6fe4d7b8771ab1e64e40ab9f334ba6
powergiant <xiaom@princeton.edu>
2026-04-30 22:55:49 -0400

    write the benchmark function

--- cs336_systems/distributed.py
+++ cs336_systems/distributed.py
@@ -4,6 +4,8 @@ import os
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
+import timeit
+
 def setup(rank: int, world_size: int):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '29500'
@@ -20,6 +22,30 @@ def distributed_demo(rank: int, world_size: int):
 def test_simple_demo():
     world_size = 4
     mp.spawn(fn = distributed_demo, args=(world_size,), nprocs= world_size, join=True)
+
+def benchmark_all_reduce(rank: int, world_size: int):
+    setup(rank, world_size)
+    times = []
+    for i in range(20):
+        tensor_to_reduce = torch.randint(0, 4, (10,)).to(device=f'cuda:{rank}')
+        if i < 5:
+            dist.all_reduce(tensor_to_reduce, async_op=False)
+            torch.cuda.synchronize()
+        else:
+            time_start = timeit.default_timer()
+            dist.all_reduce(tensor_to_reduce, async_op=False)
+            torch.cuda.synchronize()
+            time_end = timeit.default_timer()
+            times.append([i, time_start - time_end])
+    if rank == 0:
+        sum = 0
+        for i, time in times:
+            sum += time
+        print(f"Average time of all-reduce: {sum/len(times)}")
+        
+def run_benchmark_all_reduce():
+    world_size = 2
+    mp.spawn(fn=benchmark_all_reduce, args=(world_size,), nprocs=world_size, join=True)
 
 if __name__ == '__main__':
     test_simple_demo()
